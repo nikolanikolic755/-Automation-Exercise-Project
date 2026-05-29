16dc085cc65a648e213cdf4c3c0bea10d8e19086
Nikola Nikolic <nikolanikolic755@gmail.com>
2026-05-29 19:07:27 +0200

    Initial project setup

---
+++ selenium-automation/src/test/java/Base/BaseTest.java
@@ -0,0 +1,25 @@
+package Base;
+
+import Pages.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public CartPage cartPage;
+    public CheckoutPage checkoutPage;
+    public ContactUsPage contactUsPage;
+    public HomePage homePage;
+    public LoginPage loginPage;
+    public ProductsPage productsPage;
+    public SignupPage signupPage;
+
+    @BeforeClass
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+    }
+}
